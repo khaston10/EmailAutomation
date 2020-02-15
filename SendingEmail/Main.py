6a8399785535be1524cfeba0147e41cfca983b9d
Kevin Haston <kevohaston@gmail.com>
2020-02-15 14:28:33 -0800

    Updated email addr

--- SendingEmail/Main.py
+++ SendingEmail/Main.py
@@ -1,7 +1,8 @@
 import smtplib
 
 # Set email account and password
-emailAccount = 'microfarads@gmail.com '
+emailAccount = 'kevohaston@gmail.com '
+emailAccount2 = 'microfarads@gmail.com '
 password = 'Soapsoap10'
 message = 'Hi Kyle, it is me... your server.\nHope you are have a great day!\n' \
           'I am working my butt off down here.'
@@ -12,5 +13,5 @@ print(type(smtpObj))
 smtpObj.ehlo()
 smtpObj.starttls()
 smtpObj.login(emailAccount, password)
-smtpObj.sendmail(emailAccount, emailAccount, 'Subject: Salutations \n\n' + message)
+smtpObj.sendmail(emailAccount, emailAccount2, 'Subject: Salutations \n\n' + message)
 smtpObj.quit()
