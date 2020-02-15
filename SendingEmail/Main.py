c22ee9f1ead4c9d9b0ed8b16bff06f159803e531
Kevin Haston <kevohaston@gmail.com>
2020-02-15 13:48:15 -0800

    Starting new project

---
+++ SendingEmail/Main.py
@@ -0,0 +1,16 @@
+import smtplib
+
+# Set email account and password
+emailAccount = 'kevohaston@gmail.com'
+password = 'Soapsoap10'
+message = 'Hi Kyle, it is me... your server.\nHope you are have a great day!\n' \
+          'I am working my butt off down here.'
+
+smtpObj = smtplib.SMTP('smtp.gmail.com', 587)
+print(type(smtpObj))
+
+smtpObj.ehlo()
+smtpObj.starttls()
+smtpObj.login(emailAccount, password)
+smtpObj.sendmail(emailAccount, emailAccount, 'Subject: Salutations \n\n' + message)
+smtpObj.quit()
